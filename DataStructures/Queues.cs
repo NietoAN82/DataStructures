b3219867122acbda8c71a47ead82318a4a216a4a
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-04-28 07:51:37 -0500

    Updated Queues.cs

---
+++ DataStructures/Queues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DataStructures
+{
+    internal class Queues
+    {
+        public static void Main(string[] args)
+        {
+            //Initializing a queue
+            Queue<string> queue = new Queue<string>();
+            //Enqueue adds an element to the queue
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+
+            //Dequeue removes element from queue
+            System.Diagnostics.Debug.WriteLine(queue.Dequeue());
+
+            //Peek allows you to view the element at the top of the queue.
+            System.Diagnostics.Debug.WriteLine(queue.Peek());
+        }
+    }
+}
