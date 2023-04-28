4b3cd76018982d225190c3eca009a4e31a0e379f
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-04-28 08:01:36 -0500

    Adding Structs.cs

---
+++ DataStructures/Structs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Example of a struct.
+    /// </summary>
+    struct Example: INotifyPropertyChanged
+    {
+        string Ex;
+        int Ex2;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
